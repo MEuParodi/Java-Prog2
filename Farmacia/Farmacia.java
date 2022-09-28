8aa589ebce3682d68cf218808ec0ac52d9eb8ed6
Euge Parodi <112267730+MEuParodi@users.noreply.github.com>
2022-09-27 23:09:45 -0300

    Update Farmacia.java

--- Farmacia/Farmacia.java
+++ Farmacia/Farmacia.java
@@ -1,9 +1,35 @@
+/**
+ * Una farmacia desea organizar sus medicamentos. De cada uno de ellos guarda:
+ * el nombre, laboratorio, precio, un conjunto de síntomas que trata.
+ * 
+ *   La farmacia desea poder buscar todos los Medicamentos del laboratorio Bayer
+ *   Buscar medicamentos de otro laboratorio
+ *   Buscar medicamentos por nombre
+ *   Buscar medicamento por precio
+ *   Combinaciones lógicas de los anteriores
+ */
+
 package Farmacia;
 
+import java.util.ArrayList;
+
 public class Farmacia {
-
+	private ArrayList<Medicamento> medicamentos;
+	
 	public Farmacia() {
-		// TODO Auto-generated constructor stub
+		medicamentos = new ArrayList<>();
 	}
-
+	
+	public void addMedicamento(Medicamento medicamento) {
+		medicamentos.add(medicamento);
+	}
+	
+	public ArrayList<Medicamento> medicamentosFiltrados(Filtro f){
+		ArrayList <Medicamento> resultado = new ArrayList<>();
+		for (Medicamento m: medicamentos)
+			if (f.cumple(m))
+				resultado.add(m);
+		return resultado;
+	}
+	
 }
